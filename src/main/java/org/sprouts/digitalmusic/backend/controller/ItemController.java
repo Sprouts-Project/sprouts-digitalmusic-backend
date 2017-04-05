2351e58f3b52b02b91077e45efbf672c1449e317
Armando Conejero Calvo de León <Armando Conejero Calvo de Len>
2017-04-05 10:40:57 +0200

    crud item

--- src/main/java/org/sprouts/digitalmusic/backend/controller/ItemController.java
+++ src/main/java/org/sprouts/digitalmusic/backend/controller/ItemController.java
@@ -3,6 +3,7 @@ package org.sprouts.digitalmusic.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.sprouts.digitalmusic.backend.service.ItemService;
 import org.sprouts.digitalmusic.model.Item;
 import org.sprouts.digitalmusic.pojo.PaginationInfo;
+import org.sprouts.digitalmusic.pojo.ResponseObject;
 
 import io.swagger.annotations.Api;
 
@@ -24,12 +26,35 @@ public class ItemController extends AbstractController {
 	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Object[] list(@RequestParam int pageNumber) throws Exception {
 		Object[] result = new Object[2];
-		
+
 		Page<Item> page = itemService.findAll(pageNumber);
-		
+
 		result[0] = page.getContent();
 		result[1] = new PaginationInfo(pageNumber, page.getTotalPages(), page.getNumberOfElements(), 9);
-		
+
 		return result;
 	}
+
+	@RequestMapping(value = "/admin/edit", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Item edit(@RequestParam int itemId) {
+		Item item;
+
+		item = itemService.findOne(itemId);
+
+		return item;
+	}
+
+	@RequestMapping(value = "/admin/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseObject updateItem(@RequestBody Item item) throws Exception {
+		return getResponseObject(itemService.save(item));
+	}
+
+	@RequestMapping(value = "/admin/delete", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseObject deleteItem(@RequestBody Item item) throws Exception {
+		return getResponseObject(itemService.delete(item));
+	}
 }
+
+
+
+
