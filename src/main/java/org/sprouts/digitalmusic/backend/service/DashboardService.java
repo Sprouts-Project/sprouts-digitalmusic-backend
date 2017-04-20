1ab05ebc2d7361245b068c97b0697a488746a88b
David Arroyo Escobar <arroyoescobardavid@gmail.com>
2017-04-20 12:06:27 -0700

    Fixed dashboard bugs

--- src/main/java/org/sprouts/digitalmusic/backend/service/DashboardService.java
+++ src/main/java/org/sprouts/digitalmusic/backend/service/DashboardService.java
@@ -1,7 +1,12 @@
 package org.sprouts.digitalmusic.backend.service;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.List;
 
@@ -12,6 +17,7 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.sprouts.digitalmusic.model.parser.customer.CustomerOverview;
 import org.sprouts.digitalmusic.model.parser.finance.FinanceOverview;
@@ -19,6 +25,7 @@ import org.sprouts.digitalmusic.model.parser.stock.StockOverview;
 
 import com.amazonaws.util.Base64;
 import com.amazonaws.util.IOUtils;
+import com.amazonaws.util.json.JSONException;
 import com.amazonaws.util.json.JSONObject;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -110,14 +117,16 @@ public class DashboardService {
 			String response = IOUtils.toString(con.getInputStream());				
 			
 			JSONObject json = new JSONObject(response);
-			String embedded_obj = json.get("_embedded").toString();
+			String embeddedObj = json.get("_embedded").toString();
 			
 			ObjectMapper mapper = new ObjectMapper();
 			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-			List<StockOverview> array = mapper.readValue(embedded_obj,new TypeReference<List<StockOverview>>() {});
+			List<StockOverview> array = mapper.readValue(embeddedObj,new TypeReference<List<StockOverview>>() {});
 			stockOverview = array.get(0);
 				
-		} catch (Exception e) {
+		} catch (IOException e) {
+			stockOverview = new StockOverview();
+		} catch (JSONException e) {
 			stockOverview = new StockOverview();
 		}
 		return stockOverview;
@@ -132,20 +141,24 @@ public class DashboardService {
 			SSLContext sc = SSLContext.getInstance("SSL");
 			sc.init(null, trustAllCerts, new java.security.SecureRandom());
 			HostnameVerifier allHostsValid = new HostnameVerifier() {
+				@Override
 				public boolean verify(String hostname, SSLSession session) {
 					return true;
 				}
 			};
 			HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
 			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			
+		} catch (KeyManagementException e) {
+			
 		}
 	}
 
 	static class MyTrustManager implements X509TrustManager {
+		@Override
 		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-			return null;
+			return new java.security.cert.X509Certificate[0];
 		}
 
 		@Override
